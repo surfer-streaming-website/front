4042fed047b8f12502140756d2f5621a4135f4df
DESKTOP-4J4S7NL\admin <combihouse1233@gmail.com>
2024-06-20 20:34:54 +0900

    feat: artistApplication CRUD

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,15 @@ import Footer from "./components/Footer";
 import Login from "./pages/auth/login/Login";
 import Home from "./pages/Home/Home";
 import Register from "./pages/auth/register/Register";
-import ArtistApplicationsPage from "./pages/admin/artist-application-page/ArtistApplicationPage";
+import AdminArtistApplicationsPage from "./pages/admin/artist-application-page/ArtistApplicationPage";
 import Navigtion from "./components/Navigation";
 import SongBoard from "./pages/song/SongBoard";
 import AlbumBoard from "./pages/album/AlbumBoard";
-import ArtistApplicationDetail from "./pages/admin/artist-application-detail/ArtistApplicationDetail"
+import AdminArtistApplicationDetail from "./pages/admin/artist-application-detail/ArtistApplicationDetail";
+import ArtistApplicationDetail from "./pages/auth/artist-application-detail/ArtistApplicationDetail";
+import ArtistApplicationPage from "./pages/auth/artist-application-page/ArtistApplicationPage";
+import CreateArtistApplication from "./pages/auth/create-artist-application/CreateArtistApplication";
+import UpdateArtistApplication from "./pages/auth/update-artist-application/UpdateArtistApplication";
 
 export const LogingedContext = createContext();
 
@@ -35,24 +39,49 @@ function App() {
     >
       <div className="main-container">
         {/* <Header /> */}
-        <Navigtion className="navigator"/>
+        <Navigtion className="navigator" />
         <div className="main-content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route
-            path="/admin/artist-application"
-            element={<ArtistApplicationsPage />}
-          />
-          <Route path="/admin/artist-application/:id" element={<ArtistApplicationDetail/>}/>
-          <Route path="/song/detail/:id" element={<SongBoard/>}/>
-          <Route path="/album/detail/:id" element={<AlbumBoard/>}/>
+          <Routes>
+            {/* common */}
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            {/* admin */}
+            <Route
+              path="/admin/artist-application"
+              element={<AdminArtistApplicationsPage />}
+            />
+            <Route
+              path="/admin/artist-application/:id"
+              element={<AdminArtistApplicationDetail />}
+            />
+            {/* auth */}
+            <Route
+              path="/auth/artist-application"
+              element={<ArtistApplicationPage />}
+            />
+            <Route
+              path="/auth/artist-application/:id"
+              element={<ArtistApplicationDetail />}
+            />
+            <Route path="/auth/artist-application/create" element={<CreateArtistApplication />}/>
+            <Route path="/auth/artist-application/:id/update" element={<UpdateArtistApplication />}/>
+            {/* song */}
+            <Route path="/song/detail/:id" element={<SongBoard />} />
+            {/* album */}
+            <Route path="/album/detail/:id" element={<AlbumBoard />} />
 
 
-
-          <Route path="/*" element={<div><h1>404 error</h1></div>}/>
-        </Routes>
+            {/* error */}
+            <Route
+              path="/*"
+              element={
+                <div>
+                  <h1>404 error</h1>
+                </div>
+              }
+            />
+          </Routes>
         </div>
         <Footer />
       </div>
